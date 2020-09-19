9c279b0c6a85022bdb86c40c72e0a3de677eac66
Paolo Stefan <paolostefan@gmail.com>
2020-09-19 23:21:42 +0200

    entrypoint.sh in php
    crea i percorsi necessari, imposta il proprietario

--- docker/php-fpm/entrypoint.sh
+++ docker/php-fpm/entrypoint.sh
@@ -3,4 +3,9 @@ set -euo pipefail
 
 export XDEBUG_CONFIG="remote_host=$(ip route show | awk '/default/ { print $3 }')"
 
+# Per Symfony in esecuzione lato shell sotto utente developer
+mkdir -p /var/www/html/guybrush/var/log /var/www/html/guybrush/var/cache
+touch /var/www/html/guybrush/var/log/xdebug.log
+chown -R developer: /var/www/html/guybrush
+
 exec "$@"
