92f37bd44dc97717f0cc08a333b36e3aa979b7e8
Paolo Stefan <paolostefan@gmail.com>
2021-03-15 19:23:52 +0100

    Login/logout utenti statici con form

--- guybrush/src/Controller/SuperController.php
+++ guybrush/src/Controller/SuperController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SuperController extends AbstractController
@@ -10,7 +11,7 @@ class SuperController extends AbstractController
     /**
      * @Route("/super", name="super")
      */
-    public function index()
+    public function index(): Response
     {
         return $this->render(
           'super/index.html.twig',
@@ -23,7 +24,7 @@ class SuperController extends AbstractController
     /**
      * @Route("/map/empty", name="map_empty")
      */
-    public function mapEmpty()
+    public function mapEmpty(): Response
     {
         return $this->render('map_empty.html.twig');
     }
@@ -31,8 +32,16 @@ class SuperController extends AbstractController
     /**
      * @Route("/map", name="map_search")
      */
-    public function mapAction()
+    public function mapAction(): Response
     {
         return $this->render('map_search.html.twig');
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(): Response
+    {
+        return $this->render('login.html.twig');
     }
 }
