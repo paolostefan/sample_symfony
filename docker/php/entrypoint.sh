3afb05f72ea04d468bfee4331ca2dd09854ffe7e
Paolo Stefan <paolostefan@gmail.com>
2021-04-06 00:27:17 +0200

    FIX: connessione di Xdebug >=3, gateway timeout

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-export XDEBUG_CONFIG="remote_host=$(ip route show | awk '/default/ { print $3 }')"
+export XDEBUG_CONFIG="client_host=$(ip route show | awk '/default/ { print $3 }')"
 
 # Per Symfony in esecuzione lato shell sotto utente developer
 mkdir -p /var/www/html/guybrush/var/log /var/www/html/guybrush/var/cache
