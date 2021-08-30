161118bdc15b65e45142eb7922a6baa3f6e03a8f
Paolo Stefan <paolostefan@gmail.com>
2021-08-30 11:41:15 +0200

    Revert "Pacchetto email-verifier"
    This reverts commit bfd3489dd57dd7b7db343b88f8e887b5f8fbe650.

--- guybrush/config/bundles.php
+++ guybrush/config/bundles.php
@@ -13,5 +13,4 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
-    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
 ];
